0245133a8c984cea1996724c0ea1e55b4ea7132b
gathumartins <gathumartins@gmail.com>
2025-02-12 17:07:28 +0300

    updated the testimonial component

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,11 +1,55 @@
-import React from 'react'
-
+"use server";
+import React from "react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 function Testimonials() {
   return (
     <section className="py-[60px] px-[16px] bg-fshade/50">
-      <div className="myCont ">Testimonials</div>
+      <div className="myCont ">
+        <Carousel opts={{ align: "start", loop: true }}>
+          <CarouselContent className="flex items-stretch">
+            {[1, 2, 3, 4, 5, 6].map((item: any, index: number) => (
+              <CarouselItem
+                className="basis-1/2 md:basis-1/3 pl-2 md:pl-4 shadow-lg"
+                key={index}
+              >
+                <Card className="pt-[20px] bg-white flex flex-col justify-between h-full rounded-tr-3xl rounded-tl-none rounded-br-none rounded-bl-3xl">
+                  <CardContent className="body pb-0">
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Maxime quo magni fuga cupiditate, nisi facilis hic
+                      repellat aliquid ipsum cum.
+                    </p>
+                  </CardContent>
+                  <CardFooter className="statsub body flex flex-col justify-start text-left items-start">
+                    <h5>John Doe</h5>
+                    <p>CEO Doe Company</p>
+                  </CardFooter>
+                </Card>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <div className="">
+            <CarouselPrevious />
+            <CarouselNext />
+          </div>
+        </Carousel>
+      </div>
     </section>
   );
 }
 
-export default Testimonials
+export default Testimonials;
