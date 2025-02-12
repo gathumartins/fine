378d9a0dcf39005352352fad9d14b24c8cbf72ad
gathumartins <gathumartins@gmail.com>
2025-02-12 17:42:40 +0300

    updated about us page

--- src/app/(user)/about/page.tsx
+++ src/app/(user)/about/page.tsx
@@ -1,4 +1,6 @@
+import AboutSec from '@/components/AboutSec';
 import PagesHero from '@/components/PagesHero'
+import Stats from '@/components/Stats';
 import React from 'react'
 
 const page = () => {
@@ -9,6 +11,12 @@ const page = () => {
         <header className="bg-fsecondary/80 pt-[87px] w-full">
           <PagesHero />
         </header>
+      </section>
+      <section className="py-12 px-[16px]">
+        <div className="myCont py-12 px-[30px] md:px-[50px] lg:px-[80px] flex flex-col lg:flex-row justify-between gap-8 place-items-center [&_a]:hidden">
+          <AboutSec />
+          <Stats />
+        </div>
       </section>
     </article>
   );
