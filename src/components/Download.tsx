ad3f1762d57cd010f07e2fce934627aa2d157a16
gathumartins <gathumartins@gmail.com>
2025-02-14 10:50:43 +0300

    updated the single service page

--- src/components/Download.tsx
+++ src/components/Download.tsx
@@ -8,10 +8,10 @@ function Download() {
       <h3 className="mb-7 text-2xl text-fsecondary font-bold">
         Download brochure
       </h3>
-      <ul className="[&_li]:pb-2 [&_li]:mb-3 [&_li]:border-b-1 border-ftint">
+      <ul className="[&_li]:pb-2 [&_li]:mb-3 [&_li]:border-b-1 border-ftint last:[&_li]:border-b-[#f6f8fa]">
         <li className="text-xl text-fsecondary hover:text-fshade">
           <Link href="/" className="flex flex-row gap-4 items-center">
-            <FaRegFilePdf className='text-3xl inline-block'/>
+            <FaRegFilePdf className="text-3xl inline-block" />
             Company Profile
           </Link>
         </li>
--- src/components/OtherServices.tsx
+++ src/components/OtherServices.tsx
@@ -3,18 +3,26 @@ import Link from 'next/link';
 import { FaRegFilePdf } from "react-icons/fa";
 
 function OtherServices() {
-  return <div className="p-[30px] bg-[#f6f8fa]">
-    <h3 className="mb-7 text-2xl text-fsecondary font-bold">Services</h3>
+  return (
+    <div className="p-[30px] bg-[#f6f8fa]">
+      <h3 className="mb-7 text-2xl text-fsecondary font-bold">Services</h3>
 
-      <ul className="[&_li]:pb-2 [&_li]:mb-3 [&_li]:border-b-1 border-ftint">
+      <ul className="[&_li]:pb-2 [&_li]:mb-3 [&_li]:border-b-1 border-ftint last:[&_li]:border-b-[#f6f8fa]">
         <li className="text-xl text-fsecondary hover:text-fshade">
           <Link href="/" className="flex flex-row gap-4 items-center">
-            <FaRegFilePdf className='text-3xl inline-block'/>
+            <FaRegFilePdf className="text-3xl inline-block" />
+            Company Profile
+          </Link>
+        </li>
+        <li className="text-xl text-fsecondary hover:text-fshade">
+          <Link href="/" className="flex flex-row gap-4 items-center">
+            <FaRegFilePdf className="text-3xl inline-block" />
             Company Profile
           </Link>
         </li>
       </ul>
-  </div>;
+    </div>
+  );
 }
 
 export default OtherServices
