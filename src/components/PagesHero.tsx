94bf67e861d19161024cdc97853cd758c0eb3a09
gathumartins <gathumartins@gmail.com>
2025-02-19 07:57:23 +0300

    update the pages hero component bg image

--- src/components/PagesHero.tsx
+++ src/components/PagesHero.tsx
@@ -2,8 +2,14 @@ import React from "react";
 import Link from "next/link";
 
 const PagesHero = () => {
+  const bg = '/images/products.webp'
   return (
-    <section className="bg-cover bg-center bg-no-repeat w-full">
+    <section
+      className="bg-cover bg-center bg-no-repeat w-full"
+      style={{
+        backgroundImage: `url(${bg})`,
+      }}
+    >
       <div className="max-sm:pt-[24px] max-sm:pb-[28px] max-md:pt-[40px] max-md:pb-[44px] max-lg:pt-[56px] max-lg:pb-[64px] pt-[120px] pb-[70px] text-center bg-fsecondary/80 text-white">
         <div className="relative z-10">
           <h1 className="max-sm:text-3xl max-md:text-4xl text-[48px] font-semibold">
