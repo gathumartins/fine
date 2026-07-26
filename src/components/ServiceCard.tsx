adcce993def395ce8e7e0d3994ddb6448005df7d
gathumartins <gathumartins@gmail.com>
2026-07-26 20:42:56 +0300

    modified the service card

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -30,16 +30,22 @@ return (
       </CardHeader>
       <CardContent className="body px-0 pb-0 [&_h3]:text-2xl [&_h3]:font-bold relative">
         <div className="w-[70px] h-[70px] bg-fsecondary group-hover:bg-fprimary transition-all duration-500 p-3 flex justify-center place-items-center absolute z-40 -top-20 left-5">
-          <Image src={icon} alt={service.node.title} width={64} height={64} className='h-10 w-10' />
+          <Image
+            src={icon}
+            alt={service.node.title}
+            width={64}
+            height={64}
+            className="h-10 w-10 group-hover:brightness-0 transition-all duration-500"
+          />
         </div>
-        <h3 className='capitalize'>{service.node.title}</h3>
+        <h3 className="capitalize">{service.node.title}</h3>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, ex?
         </p>
         <p>{service.node.excerpt}</p>
       </CardContent>
-      <CardFooter className="[&_button]:underline [&_button]:text-fsecondary [&_button]:text-xl [&_button]:font-bold px-0 pt-0">
-        <Button className="group-hover:text-fprimary transition-all duration-500 bg-transparent rounded-none hover:bg-transparent text-xl p-0">
+      <CardFooter className="[&_button]:text-fsecondary [&_button]:text-xl [&_button]:font-bold px-0 pt-0">
+        <Button className="group-hover:text-fprimary transition-all duration-500 bg-transparent bg-fprimary group-hover:bg-fsecondary rounded-none hover:bg-transparent text-xl py-3 px-4">
           See More
         </Button>
       </CardFooter>
