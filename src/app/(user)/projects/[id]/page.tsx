ea1ccd129d294f4d13032e8cb6e623cf2acbd530
gathumartins <gathumartins@gmail.com>
2026-07-28 20:13:04 +0300

    updated the single project page

--- src/app/(user)/projects/[id]/page.tsx
+++ src/app/(user)/projects/[id]/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import Image from 'next/image';
 import ProjectInfo from '@/components/ProjectInfo';
 
-async function page() {
+async function page({ params }: { params: Promise<{ id: string }> }) {
 const query = `{
   page:page(id: "cG9zdDoyMA==", idType: ID) {
     minibanner {
@@ -17,13 +17,48 @@ const query = `{
       }
     }
   }
-
+projects:projects {
+    edges {
+      node {
+        id
+        slug
+        title
+        content
+        featuredImage {
+          node {
+            altText
+            mediaDetails {
+              height
+              width
+            }
+            mediaItemUrl
+          }
+        }
+        projectFields {
+          information {
+            title
+            fields {
+              key
+              value
+            }
+          }
+        }
+      }
+    }
+  }
 }`;
 const result = await fetch(
   `${process.env.WORDPRESS_API_URL}?query=${encodeURIComponent(query)}`,
   { headers: { "Content-Type": "application/json" } }
 );
+const resolvedParams = await params;
+const slug = resolvedParams.id;
 const data = await result.json();
+const allProjects = data.data.projects.edges;
+const project = allProjects.find((p: any) => p.node.slug === slug);
+const otherProjects = allProjects.filter((p: any) => p.node.slug !== slug);
+const topimageUrl = project.node.featuredImage?.node?.mediaItemUrl || "/images/projectdetails.webp";
+const projectInfo = project.node.projectFields?.information;
   return (
     <article className="page">
       <section className="">
@@ -34,40 +69,17 @@ const data = await result.json();
       <section className="py-12 px-[16px]">
         <div className="myCont">
           <Image
-            src="/images/projectdetails.webp"
+            src={topimageUrl}
             alt="Fine Work Kenya Limited Project"
             width={1170}
             height={439}
           />
           <div className="body stats flex flex-col lg:flex-row gap-8 mt-8">
             <aside className="lg:basis-1/3">
-              <ProjectInfo />
+              <ProjectInfo fields={projectInfo} />
             </aside>
             <main className="lg:basis-2/3">
-              <h4>Mountain Tunnel</h4>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Voluptatem veritatis quo et ullam, ducimus itaque earum dolorem?
-                Consectetur, et, aut. A, corporis officia eius dicta explicabo
-                saepe nesciunt, mollitia minima, atque maiores optio cum. Atque
-                amet unde impedit voluptate cumque distinctio minima, aspernatur
-                nemo! Expedita in, numquam blanditiis ullam rem!
-              </p>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi
-                cum fugit officia dolores eligendi, rem. Quibusdam quasi impedit
-                perspiciatis iure maiores, eaque numquam doloremque, quo nam
-                soluta itaque obcaecati tempore!.
-              </p>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore
-                ex, nam adipisci dolores laborum earum. Unde cum, ut nostrum
-                nihil alias, laudantium molestiae, vitae quidem dolorem officiis
-                ipsum. Aliquid nemo consequuntur cupiditate delectus sapiente
-                doloribus dolorem, at suscipit, non laudantium mollitia magnam
-                repellat atque quia! Aut, veniam, nam. Ex porro optio facilis
-                nostrum, qui ipsa?
-              </p>
+              <div className="" dangerouslySetInnerHTML={{ __html: project?.node.content || "" }} />
             </main>
           </div>
           <footer className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 mt-5">
